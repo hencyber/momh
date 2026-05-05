b6d10fb5afa71dceece7b04ef8dfb94dd0678af2
Mona Abdi <mona.abdi9085@gmail.com>
2026-05-05 19:48:09 +0200

    Remove unused imports, add retrieval, metadata extraction, and structured return

---
+++ frontend/app_monatest.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import requests
+
+
+st.title("Utlandsstudier Chat")
+
+user_input= st.text_input("Ställ dina frågor kring utlandsstudier")
+
+if st.button("Skicka"):
+    if user_input:
+        response = requests.post(
+            "http://localhost:8003/chat",
+            json={"question":user_input}
+        )
+
+        if response.status_code == 200:
+            data=response.json()
+
+            st.subheader("Svar:")
+            st.write(data["answer"])
+            st.subheader("källor:")
+            for source in data ["sources"]:
+                 st.write("-"+ source)
+        else:
+             st.error("Error, API-call failed")
+
+
+
+
+
+
+    
